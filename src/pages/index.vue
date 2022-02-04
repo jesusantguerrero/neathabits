1a5be8e1004979ee9e9f878ff09a254d4bcf546b
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-02-04 07:37:04 -0400

    🚧 WIP: draft tag lines on hero

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -32,5 +32,13 @@ const state = {
       :links="state.links"
       :actions="state.actions"
     />
+    <div class="relative max-w-5xl mx-auto py-20 sm:pt-24 lg:pt-32">
+      <h1 class="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+        The tool to track your daily routine and habits like a pro
+      </h1>
+      <p class="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
+        Based on <span class="text-primary-400 font-bold">Atomic habits</span> and <span class="text-primary-400 font-bold">Shape Up</span>
+      </p>
+    </div>
   </div>
 </template>
