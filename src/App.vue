4d9a5a553324e8288bfab4da70c052e83c9d9d1d
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-02-04 06:44:14 -0400

    🔥 chore: removing template marks

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'Neathabits',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'The habit tracker you need' },
   ],
 })
 </script>
