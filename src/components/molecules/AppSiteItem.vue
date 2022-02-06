a3f9d6d8bbac1ea426c048318d40ab95a0c8bac7
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-02-05 20:24:53 -0400

    💄 fix: improve SiteItem styles

--- src/components/molecules/AppSiteItem.vue
+++ src/components/molecules/AppSiteItem.vue
@@ -8,9 +8,9 @@ defineProps<{ item: ISideItem }>()
 </script>
 
 <template>
-  <router-link :to="item.to">
-    <div class="flex items-center py-3 pl-5 font-semibold text-primary">
-      <div v-if="item.icon" class="flex items-center justify-center w-6 h-6 mr-2 text-sm rounded-md bg-primary-100">
+  <router-link :to="item.to" class="group">
+    <div class="flex items-center py-3 pl-5 font-semibold group-hover:text-primary">
+      <div v-if="item.icon" class="flex items-center justify-center w-8 h-8 mr-2 text-sm transition bg-gray-100 border-2 rounded-md side-item group-hover:bg-primary-400 group-hover:text-white">
         <component :is="item.icon" />
       </div>
       <span>
@@ -19,3 +19,13 @@ defineProps<{ item: ISideItem }>()
     </div>
   </router-link>
 </template>
+
+<style lang="scss" scoped>
+.router-link-exact-active {
+  @apply text-primary;
+
+  .side-item {
+    @apply bg-primary-400 text-white font-bold border-primary-200;
+  }
+}
+</style>
