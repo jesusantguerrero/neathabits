6555f96501109ec1d65c04b4921b807e2517023f
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-02-06 09:46:10 -0400

    WIP: Draft habits template

---
+++ src/components/organisms/HabitItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface IEvent {
+  title: string;
+  description: string;
+  startTime: Date;
+  endTime: Date;
+}
+defineProps<{
+  habit: IEvent;
+}>();
+</script>
+
+<template>
+  <article class="rounded-md w-full border px-5 py-2">
+    <h4 class="font-semibold">
+      {{ habit.title }}
+    </h4>
+    <p>{{ habit.description }}</p>
+    <small> {{ habit.startTime }} - {{ habit.endTime }} </small>
+  </article>
+</template>
---
+++ src/pages/dashboard/Habits.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import HabitsTemplate from '~/components/templates/HabitsTemplate.vue'
+
+const state = reactive({
+  habits: [],
+  labels: [],
+})
+</script>
+
+<template>
+  <HabitsTemplate v-bind="state" />
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+  requiresAuth: false
+</route>
