288058f6e4866fb1f0fb8ec38cdda67782bba16c
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-02-08 09:07:24 -0400

    feat: add vite to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const Icons = require('unplugin-icons/vite');
+const IconsResolver = require('unplugin-icons/resolver');
+const Components = require('unplugin-vue-components/vite');
+const AutoImport = require('unplugin-auto-import/vite');
 
 module.exports = {
   "stories": [
@@ -18,7 +22,56 @@ module.exports = {
     }
   ],
   "framework": "@storybook/vue3",
-   webpackFinal: async (config) => {
+  core: {
+    "builder": "storybook-builder-vite",
+  },
+  async viteFinal(config) {
+    config.resolve.alias['~/'] = `${path.resolve(__dirname, '..', 'src')}/`;
+
+    config.plugins = [...config.plugins,
+        // https://github.com/antfu/unplugin-icons
+        Icons({
+          autoInstall: true,
+        }),
+
+        // https://github.com/antfu/unplugin-auto-import
+        AutoImport({
+          imports: [
+            'vue',
+            'vue-router',
+            'vue-i18n',
+            '@vueuse/head',
+            '@vueuse/core',
+            'vitest',
+          ],
+          dts: 'src/auto-imports.d.ts',
+        }),
+
+        // https://github.com/antfu/unplugin-vue-components
+        Components({
+          // allow auto load markdown components under `./src/components/`
+          extensions: ['vue', 'md'],
+
+          // allow auto import and register components used in markdown
+          include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+
+          // custom resolvers
+          resolvers: [
+            // auto import icons
+            // https://github.com/antfu/unplugin-icons
+            IconsResolver({
+              prefix: false,
+              // enabledCollections: ['carbon']
+            }),
+          ],
+
+          dts: 'src/components.d.ts',
+        }),
+    ];
+
+    return config;
+  },
+  webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
       use: [
